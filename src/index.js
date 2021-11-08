b6048c87663a8fdbe43cf7c2fcad93d8da8cda2f
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2021-11-08 14:35:33 -0300

    Tentando

--- src/index.js
+++ src/index.js
@@ -22,21 +22,24 @@ app.post("/repositories", (request, response) => {
     techs,
     likes: 0
   };
+  repositories.push(repository);
+  return response.status(201).send()
 
-  return response.json(repository);
+  //return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
+  
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
 
+  const repositoryIndex = repositories.findindex(repository => repository.id === id);
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
+  const repository = { ...repositories[repositoryIndex], ...newUpdatedRepository };
 
   repositories[repositoryIndex] = repository;
 
