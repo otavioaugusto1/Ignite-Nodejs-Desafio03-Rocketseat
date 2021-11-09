861cd500b384fb190ff26d90aa046a12606a5d25
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2021-11-09 08:20:44 -0300

    update

--- src/index.js
+++ src/index.js
@@ -23,23 +23,26 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
   repositories.push(repository);
-  return response.status(201).send()
+  return response.status(201).json(repos)
 
   //return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
+  const { title, url, techs} = request.body;
   
-
-
   const repositoryIndex = repositories.findindex(repository => repository.id === id);
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const repository = { ...repositories[repositoryIndex], ...newUpdatedRepository };
+  const updatedRepository = {
+    title,
+    url,
+    techs
+  }
+  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
 
   repositories[repositoryIndex] = repository;
 
@@ -69,7 +72,7 @@ app.post("/repositories/:id/like", (request, response) => {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const likes = ++repositories[repositoryIndex].likes;
+  repositories[repositoryIndex].likes += 1;
 
   return response.json('likes');
 });
